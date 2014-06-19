a8d78d425913ecf294fb26745847caf05564ff72
Mohamad Mohebifar <netfars@gmail.com>
2014-06-19 18:58:34 +0430

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,11 +19,8 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('mohebifar_calendar');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
+        $rootNode->children()->scalarNode('driver');
+        
         return $treeBuilder;
     }
 }
